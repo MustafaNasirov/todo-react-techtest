cc4eb35221a43e2329c3b2264150478304ab4ee9
MustafaNsrv <86935457+MustafaNasirov@users.noreply.github.com>
2022-07-28 19:14:18 +0400

    finished basic styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import "./Nav.scss"
 const Nav = ({reset}) => {
     return(
         <div className="nav__container">
-            <h1 className="nav__title">To Dos</h1>
+            <h1 className="nav__title">My To Dos</h1>
             <button className="nav__resetBtn" onClick={reset}>Reset</button>
         </div>
     )
